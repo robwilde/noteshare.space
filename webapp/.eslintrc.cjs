a689f17a97d23c03e04a9d6ece27d6d058a3ce3d
mrwilde <robert@mrwilde.com>
2026-03-11 15:48:26 +1000

    chore: remove pnpm-lock.yaml
    - Deleted `pnpm-lock.yaml` to clean up unused or redundant lock file.
    - This ensures a consistent dependency management strategy in the repository.

--- webapp/.eslintrc.cjs
+++ webapp/.eslintrc.cjs
@@ -1,13 +1,26 @@
 module.exports = {
 	root: true,
 	parser: '@typescript-eslint/parser',
-	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
-	plugins: ['svelte3', '@typescript-eslint'],
-	ignorePatterns: ['*.cjs'],
-	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
-	settings: {
-		'svelte3/typescript': () => require('typescript')
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:svelte/recommended',
+		'prettier'
+	],
+	plugins: ['@typescript-eslint'],
+	rules: {
+		'svelte/no-at-html-tags': 'off'
 	},
+	ignorePatterns: ['*.cjs'],
+	overrides: [
+		{
+			files: ['*.svelte'],
+			parser: 'svelte-eslint-parser',
+			parserOptions: {
+				parser: '@typescript-eslint/parser'
+			}
+		}
+	],
 	parserOptions: {
 		sourceType: 'module',
 		ecmaVersion: 2020
